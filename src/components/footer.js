d7a4b14193c5d93881ffd37f6e1983175f7209f5
jake <jdineen@asu.edu>
2024-03-11 10:38:59 -0700

    fix logo and get in touch section

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { Icon } from '@components/icons';
-import { socialMedia } from '@config';
+import React from "react"
+import styled from "styled-components"
+import { Icon } from "@components/icons"
+import { socialMedia } from "@config"
 
 const StyledFooter = styled.footer`
   ${({ theme }) => theme.mixins.flexCenter};
@@ -11,7 +10,7 @@ const StyledFooter = styled.footer`
   min-height: 70px;
   padding: 15px;
   text-align: center;
-`;
+`
 
 const StyledSocialLinks = styled.div`
   display: none;
@@ -38,7 +37,7 @@ const StyledSocialLinks = styled.div`
       }
     }
   }
-`;
+`
 
 const StyledCredit = styled.div`
   color: var(--light-slate);
@@ -65,29 +64,9 @@ const StyledCredit = styled.div`
       height: 14px;
     }
   }
-`;
-
+`
 const Footer = () => {
-  const [githubInfo, setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
-
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      return;
-    }
-    fetch('https://api.github.com/repos/bchiang7/v4')
-      .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
-      .catch(e => console.error(e));
-  }, []);
+  // Removed the useState hook for setGitHubInfo since it's not being used
 
   return (
     <StyledFooter>
@@ -106,28 +85,18 @@ const Footer = () => {
 
       <StyledCredit tabindex="-1">
         <a href="https://github.com/jacobdineen/jacobdineen/">
-          <div>Built by Jacob Dineen with heavy design inspiration from Brittany Chiang</div>
-
-          {githubInfo.stars && githubInfo.forks && (
-            <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <Icon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </div>
-          )}
+          <div>
+            Built by Jacob Dineen with heavy design inspiration from Brittany
+            Chiang
+          </div>
         </a>
       </StyledCredit>
     </StyledFooter>
-  );
-};
+  )
+}
 
 Footer.propTypes = {
-  githubInfo: PropTypes.object,
-};
+  // Removed the unused propType for githubInfo
+}
 
-export default Footer;
+export default Footer
