1363e9343b9636f8c26d01e34ef5e721c18e3e9f
jake <jdineen@asu.edu>
2024-08-02 11:06:55 -0700

    fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -214,7 +214,7 @@ const Layout = ({ children, location }) => {
   const handleSectionClick = (e, section) => {
     e.preventDefault();
     if (section === 'resume') {
-      window.location.href = '/Academic_Resume2.pdf';
+      window.location.href = '/jacobdineen_resume.pdf';
     } else {
       document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
       setActiveSection(section);
